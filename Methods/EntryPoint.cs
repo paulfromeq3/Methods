b8e9a61a919018f122a1812566f80434b5bf29f8
paulfromeq3 <paul.walker@eq3.org>
2021-07-19 17:01:03 -0500

    Introduction to Methods section

--- Methods/EntryPoint.cs
+++ Methods/EntryPoint.cs
@@ -1,8 +1,31 @@
 ﻿using System;
 
-    class EntryPoint
+class EntryPoint
+{
+    static void Main()
     {
-        static void Main()
-        {
-        }
+        Console.WriteLine("Before the Methods");
+        PrintingNames();
+        PrintingNames();
+        Console.WriteLine("In between the Methods");
+        PrintingNames();
+        PrintingNames();
+        Console.WriteLine("After the Methods");
     }
+
+    // Code here is outside of the Main method
+    // This is where your methods will go.
+    // Naming convention for Methods: all Methods start with a capital letter.
+    // Methods should do only one thing.
+    // Methods look like variables with parenthesis at the end.
+    // After the Method is executed, code execution will continue with the line after the call to the Method.
+
+
+    static void PrintingNames()
+    {
+        Console.Write("My name is Paul Walker ");
+        Console.Write("and I am ");
+        Console.Write("learning C#\n");
+    }
+
+}

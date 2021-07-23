3076c17ae7d3c5999a59bfe7707f8210119e26bf
paulfromeq3 <paul.walker@eq3.org>
2021-07-23 00:55:37 -0500

    Methods with unlimited variables.

--- 05.VarNumOfArgs/EntryPoint.cs
+++ 05.VarNumOfArgs/EntryPoint.cs
@@ -5,6 +5,40 @@ class EntryPoint
 {
     static void Main()
     {
+        // Methods that can take an unlimited number of arguments
+        // They have to be the same type variables
+        // First example takes integers
+        // Just numbers, no special arguments
+        // Must use "params" argument before the type
+        // NOTE: you CAN create an array Method without useing the "params" keyword, but you have to use an array as input
+        // In the example below, we use the "params" keyword and just give it a bunch of integers.
+        // Since there could be any number of values, the type will be an array of integers
+        // Last you need the variable to which you're going to assign the values
+
+        Console.WriteLine(Sum(1, 3, 4, 5, 7, 8, 8, 44, 55, 44, 33, 22, 6, 4, 3, 3, 2, 4, 5));
+        // You could also rename this Method something like "Perimeter()" and feed it the sides of the object you want the perimeter for.
+        // It could be an any-sided object, it would still work.
+
+        // I usually use interpolation, but...
+        // Using Placeholders
+
+        int firstNumber = 5;
+        int secondNumber = 7;
+        int thirdNumber = 9;
+
+        Console.WriteLine("{0} {1} {2}", firstNumber, secondNumber, thirdNumber);
+
+    }
+
+    static int Sum(params int[] numbers)
+    {
+        // Since you don't know how many numbers there are, you need to iterate through them with a for or foreach loop
+        int sum = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            sum += numbers[i];
+        }
+        return sum;
 
     }
 }

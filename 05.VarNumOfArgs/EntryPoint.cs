b06877906eb63a6a8a233a4d2b09f0d1ddb654ed
paulfromeq3 <paul.walker@eq3.org>
2021-07-23 22:44:35 -0500

    More work on Methods with variable number of arguments

--- 05.VarNumOfArgs/EntryPoint.cs
+++ 05.VarNumOfArgs/EntryPoint.cs
@@ -15,30 +15,70 @@ class EntryPoint
         // Since there could be any number of values, the type will be an array of integers
         // Last you need the variable to which you're going to assign the values
 
-        Console.WriteLine(Sum(1, 3, 4, 5, 7, 8, 8, 44, 55, 44, 33, 22, 6, 4, 3, 3, 2, 4, 5));
+        //Console.WriteLine(Sum(1, 3, 4, 5, 7, 8, 8, 44, 55, 44, 33, 22, 6, 4, 3, 3, 2, 4, 5));
         // You could also rename this Method something like "Perimeter()" and feed it the sides of the object you want the perimeter for.
         // It could be an any-sided object, it would still work.
 
         // I usually use interpolation, but...
         // Using Placeholders
 
-        int firstNumber = 5;
-        int secondNumber = 7;
-        int thirdNumber = 9;
+        //int firstNumber = 5;
+        //int secondNumber = 7;
+        //int thirdNumber = 9;
 
-        Console.WriteLine("{0} {1} {2}", firstNumber, secondNumber, thirdNumber);
+        //Console.WriteLine("{0} {1} {2}", firstNumber, secondNumber, thirdNumber);
+
+        // Challenge 1
+        // Make your own version of the WriteLine Method to use placeholders
+        // Basically recreate it in your own way
+        // I guess use [] square brackets to separate the numbers
+
+        string firstWord = "Trump";
+        string secondWord = "WON!";
+        string thirdWord = "Biden";
+        string fourthWord = "LOST!";
+        MyWriteLine("[0] [2] [1] [3]", firstWord, secondWord, thirdWord, fourthWord);
+
+
+
+
+
 
     }
 
-    static int Sum(params int[] numbers)
+    static void MyWriteLine(string text, params string[] variables)
     {
-        // Since you don't know how many numbers there are, you need to iterate through them with a for or foreach loop
-        int sum = 0;
-        for (int i = 0; i < numbers.Length; i++)
+
+        string newText = string.Empty;
+        for (int i = 0; i < text.Length; i++)
         {
-            sum += numbers[i];
+            newText += text[i];
         }
-        return sum;
+        Console.WriteLine(newText);
+
+        // if you take a character of text like text[i], you get a char type
+        // convert it to string with the ToString() Method if you want to use it
+        for (int i = 0; i < text.Length; i++)
+        {
+            if ((text[i].ToString() != "[") && (text[i].ToString() != "]") && (text[i].ToString() != " "))
+            {
+                //Console.WriteLine(text[i]);
+                Console.WriteLine(variables[Convert.ToInt32(text[i])]);
+            }
+        }
+
 
     }
+
+    //static int Sum(params int[] numbers)
+    //{
+    //    // Since you don't know how many numbers there are, you need to iterate through them with a for or foreach loop
+    //    int sum = 0;
+    //    for (int i = 0; i < numbers.Length; i++)
+    //    {
+    //        sum += numbers[i];
+    //    }
+    //    return sum;
+
+    //}
 }
